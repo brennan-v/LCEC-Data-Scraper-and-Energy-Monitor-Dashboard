faa200a62a1144f43007c18cda5cce6ec9e2627c
Brennan Vargas <me@brennanv.com>
2018-05-22 17:01:38 -0400

    First version of energy usage db table as a migration
    Removed un-used migrations and created first concept of energy_usage table.

--- database/migrations/2018_05_22_042355_create_energy_usage_table.php
+++ database/migrations/2018_05_22_042355_create_energy_usage_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateEnergyUsageTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('energy_usage', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedInteger('kwh_used');
+            $table->unsignedDecimal('cost');
+            $table->Integer('high_temp');
+            $table->Integer('low_temp');
+            $table->Integer('average_temp');
+            $table->date('date')->unique();
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('energy_usage');
     }
 }
